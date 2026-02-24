f18af59ca27880379a86b0cc2953c31b697c2d2c
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-24 13:16:25 -0300

    Criando métodos http

--- routes/livros.js
+++ routes/livros.js
@@ -3,7 +3,23 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("olá mundo let linda");
+  try {
+    res.send("olá mundo let linda");
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro no servidor");
+  }
+});
+
+router.post("/", (req, res) => {
+  res.send("Você fez um POST para /livros");
+});
+
+router.put("/", (req, res) => {
+  res.send("Você fez um PUT para /livros");
+});
+
+router.delete("/", (req, res) => {
+  res.send("Você fez um DELETE para /livros");
 });
 
 module.exports = router;
