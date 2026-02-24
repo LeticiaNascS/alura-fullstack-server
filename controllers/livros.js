5ada06b34ce696e51c58e86bc39e3ef69b1ee0b1
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-24 15:46:11 -0300

    Lidando com parametros

--- controllers/livros.js
+++ controllers/livros.js
@@ -1,4 +1,4 @@
-const { getTodosLivros } = require("../server/livros");
+const { getTodosLivros, getLivroPorId } = require("../server/livros");
 
 function getLivros(req, res) {
   try {
@@ -9,6 +9,17 @@ function getLivros(req, res) {
   }
 }
 
+function getLivro(req,res){
+     try {
+    const id =  req.params.id
+    const livro = getLivroPorId(id);
+    res.send(livro);
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro no servidor");
+  }
+}
+
 module.exports = {
   getLivros,
+  getLivro
 };
